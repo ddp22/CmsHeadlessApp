9209289d605540431b9f9af5756d43bd06d2824e
DOMENICO PIO D'AMORA <domypio001@gmail.com>
2022-08-22 12:23:11 +0200

    Speriamo

---
+++ CmsHeadlessApp/Platforms/Android/SimpleArActivity.cs
@@ -0,0 +1,58 @@
+﻿/*using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Com.Wikitude.Architect;
+using Com.Wikitude.Common.Camera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsHeadlessApp.Platforms.Android
+{
+    [Activity(Label = "SimpleArActivity", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class SimpleArActivity : Activity
+    {
+        public ArchitectView architectView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var config = new ArchitectStartupConfiguration
+            {
+                LicenseKey = "YOUR-LICENSE-KEY",
+                CameraPosition = CameraSettings.CameraPosition.Back,
+                CameraResolution = CameraSettings.CameraResolution.FULLHD1920x1080,
+                CameraFocusMode = CameraSettings.CameraFocusMode.Continuous,
+                ArFeatures = ArchitectStartupConfiguration.Features.ImageTracking
+            };
+
+            architectView = new ArchitectView(this);
+            architectView.OnCreate(config);
+
+            SetContentView(architectView);
+        }
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+            architectView.OnPostCreate();
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            architectView.OnResume();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            architectView.OnPause();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            architectView.OnDestroy();
+        }
+    }
+}*/
